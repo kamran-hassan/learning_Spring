a8367baa4c9e9cf9798366ec1e27c932f11fba47
kamran-hassan <hassan.kamran107@gmail.com>
2022-10-22 14:42:09 +0530

    first Commit

---
+++ src/main/java/com/kamran/hassan/learningspring/LearningSpringApplication.java
@@ -0,0 +1,13 @@
+package com.kamran.hassan.learningspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kamran/hassan/learningspring/LearningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamran.hassan.learningspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
