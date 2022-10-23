b9c32197c7623457aee0d3bd83719ec81273e694
kamran-hassan <hassan.kamran107@gmail.com>
2022-10-23 06:30:45 +0530

    crud REPO Comment added See RoomRepository.java

--- src/main/java/com/kamran/hassan/learningspring/data/RoomRepository.java
+++ src/main/java/com/kamran/hassan/learningspring/data/RoomRepository.java
@@ -3,6 +3,6 @@ package com.kamran.hassan.learningspring.data;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository  // Indicates that an annotated class is a "Repository", originally defined by Domain-Driven Design (Evans, 2003) as "a mechanism for encapsulating storage, retrieval, and search behavior which emulates a collection of objects".
 public interface RoomRepository extends CrudRepository<Room, Long> {
 }
