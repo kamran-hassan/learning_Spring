c504f2f8ad08c5adefc3e8fd01c302b5257a1ae0
kamran-hassan <hassan.kamran107@gmail.com>
2022-10-26 07:29:13 +0530

    made a controller with a model, controller methode will return string(name of model instead of complete model)

---
+++ src/main/java/com/kamran/hassan/learningspring/web/RoomReservationController.java
@@ -0,0 +1,29 @@
+package com.kamran.hassan.learningspring.web;
+
+
+import com.kamran.hassan.learningspring.business.ReservationService;
+import com.kamran.hassan.learningspring.business.RoomReservation;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/reservation")
+public class RoomReservationController {
+    private final ReservationService reservationService;
+
+    public RoomReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String getReservation(@RequestParam(value = "date", required = false) String dateString, Model model){
+        List<RoomReservation> reservations = this.reservationService.getAllReservationDetailByDateString(dateString);
+        model.addAttribute("roomReservation", reservations);
+        return "roomres";   // will be get clear after some time
+    }
+}
